9bdd0978147bcc0f7ee47f234cbbb409e63b8433
MNathanM <mcelroy6@illinois.edu>
2025-04-12 13:14:42 -0500

    Added bounds for the camera for different screen ratios

--- Assets/Scripts/Camera_Movement.cs
+++ Assets/Scripts/Camera_Movement.cs
@@ -9,6 +9,9 @@ public class Camera_Movement : MonoBehaviour
     [SerializeField] private Transform terrain_image;
 
     [SerializeField] private float camera_offset = 9f;
+    [SerializeField] private float camera_offset_16x9 = 9f;
+    [SerializeField] private float camera_offset_16x10 = 8.1f;
+    [SerializeField] private float camera_offset_other = 9.75f;
 
     private float terrain_image_right;
     private float terrain_image_left;
@@ -24,6 +27,22 @@ public class Camera_Movement : MonoBehaviour
     {
         if (player_position.position.x < terrain_image_right && player_position.position.x > terrain_image_left) {
           transform.position = new Vector3(player_position.position.x, player_position.position.y, -10);
+        }
+
+        Vector2 resolution = new Vector2(Screen.width, Screen.height);
+        float ratio = resolution.x/resolution.y;
+        if (ratio - 16f/10f <= 0.001) {
+            camera_offset = camera_offset_16x10;
+            terrain_image_right = 9*terrain_image.localScale.x - camera_offset;
+            terrain_image_left = -9*terrain_image.localScale.x + camera_offset;
+        } else if (ratio - 16f/9f <= 0.001) {
+            camera_offset = camera_offset_16x9;
+            terrain_image_right = 9*terrain_image.localScale.x - camera_offset;
+            terrain_image_left = -9*terrain_image.localScale.x + camera_offset;
+        } else {
+            camera_offset = camera_offset_other;
+            terrain_image_right = 9*terrain_image.localScale.x - camera_offset;
+            terrain_image_left = -9*terrain_image.localScale.x + camera_offset;
         }
     }
 }
