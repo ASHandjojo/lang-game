5bcb91e62ca08c3d802289d7ecb45f82da9e8806
Matthew M. <blazinggunner@hotmail.com>
2025-04-26 18:29:24 -0500

    Added ligature table parsing!!

--- Assets/Scripts/Encoding/Loader/LigatureSub.cs
+++ Assets/Scripts/Encoding/Loader/LigatureSub.cs
@@ -1,6 +1,110 @@
 using System;
-using System.Text;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.TextCore;
 
-public 
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[Serializable]
+public sealed class LigatureSubJSON
+{
+    public ushort Glyph { get; set; }
+    public ushort[] Components { get; set; }
+}
+
+[Serializable]
+public sealed class LigatureSubEntry
+{
+    public ushort Glyph;
+    public ushort[] Components;
+
+    public LigatureSubEntry(LigatureSubJSON json)
+    {
+        Glyph      = json.Glyph;
+        Components = json.Components;
+    }
+    public override string ToString()
+    {
+        string output = $"Glyph: {Glyph} ";
+        foreach (ushort component in Components)
+        {
+            output += $"{component},";
+        }
+        return output;
+    }
+}
+
+[Serializable]
+public sealed class LigatureSubData
+{
+    public LigatureSubEntry[] entries;
+
+    public LigatureSubData(LigatureSubJSON[] json)
+    {
+        entries = json.Select(y => new LigatureSubEntry(y)).ToArray();
+    }
+
+    public override string ToString()
+    {
+        string output = "";
+        foreach (LigatureSubEntry entry in entries)
+        {
+            output += $"{entry}\n";
+        }
+        return output;
+    }
+}
+
+[CreateAssetMenu(fileName = "Ligature Table")]
+public sealed class LigatureSub : ScriptableObject
+{
+    [SerializeField] private string fileDir;
+    [SerializeField, HideInInspector] public LigatureSubData[] entries;
+
+    public string FileDir => fileDir;
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(LigatureSub))]
+public sealed class LigatureSubEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LigatureSub table = target as LigatureSub;
+
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        serializedObject.Update();
+        bool hasChanges = EditorGUI.EndChangeCheck();
+
+        if (hasChanges)
+        {
+            using FileStream stream = new(table.FileDir, FileMode.Open);
+
+            LigatureSubJSON[][]? ligatureNested = JsonSerializer.Deserialize<LigatureSubJSON[][]>(stream);
+            Debug.Assert(ligatureNested != null);
+
+            var ligatures = ligatureNested.Select(x => new LigatureSubData(x)).ToArray();
+            table.entries = ligatures;
+
+            foreach (var ligature in ligatures)
+            {
+                Debug.Log(ligature);
+            }
+
+            EditorUtility.SetDirty(table);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        GUILayout.Label($"Length: {table.entries.Length}", EditorStyles.boldLabel);
+    }
+}
+#endif
