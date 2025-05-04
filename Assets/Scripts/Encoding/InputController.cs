e0e16360817bf9bc48a450dfc68b0998e619945d
Matthew M. <blazinggunner@hotmail.com>
2025-05-04 12:57:55 -0500

    **Hopefully** fixed behavior, using a much more aggressively configured converted ligature set that unpacks, sorts, and repacks ligature sets to eliminate the problem of out of order, non-unique entries in the ligature sets in the ligature substitution table

--- Assets/Scripts/Encoding/InputController.cs
+++ Assets/Scripts/Encoding/InputController.cs
@@ -21,7 +21,7 @@ public sealed class InputController : MonoBehaviour
         processor = new Processor(LanguageTable.StandardSigns, LanguageTable.CompoundSigns);
 
         inputField = document.rootVisualElement.Q<Label>("Input");
-        InputStr   = processor.Translate("aeeiio+qo+");
+        InputStr   = processor.Translate("aeeiio+qo+ra++we");
     }
 
     void OnDestroy()
