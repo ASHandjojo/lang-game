a531537fecd00829024e51d31685187ed47b1611
yyaasshh1018 <coolyg1018@gmail.com>
2026-04-05 19:03:51 -0500

    Notebook can now be opened while in dialogue with NPCs

--- Assets/Scripts/Interactables/NpcDialogue.cs
+++ Assets/Scripts/Interactables/NpcDialogue.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 using Unity.Collections;
 
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.EventSystems.EventTrigger;
 
 [DisallowMultipleComponent, RequireComponent(typeof(UIDocument))]
 public class NpcDialogue : Interactable
@@ -33,6 +35,24 @@ public class NpcDialogue : Interactable
 
     public Font conlangFont;
     public Font englishFont;
+
+    private VisualElement notebookContents;
+    private Button notebookButton;
+
+    private Button backButton;
+    private Button dictionaryButton;
+    private Button journalButton;
+    private Label pageCount;
+
+    private Button backPage;
+    private Button forwardPage;
+
+    private List<VisualElement> Slots = new List<VisualElement>();
+    private int pageNumber = 0;
+
+    private TextField journalPage;
+
+    private bool journalOrDict = true; // true: dictionary, false: journal, will open by default
 
     public override PlayerContext TargetContext { get => PlayerContext.Interacting | PlayerContext.Dialogue; }
 
@@ -74,6 +94,60 @@ public class NpcDialogue : Interactable
 
         // Set Tooltip
         wordTooltip = document.rootVisualElement.Q("WordTooltip");
+
+        // Setup Notebook
+        notebookButton = document.rootVisualElement.Q<Button>("NotebookButton");
+        notebookButton.RegisterCallback<ClickEvent>((e) => ToggleNotebook());
+
+        notebookContents = document.rootVisualElement.Q<VisualElement>("Notebook");
+
+        for (int i = 0; i < 5; i++)
+        {
+            int slotNumber = i + 1;
+            var item = notebookContents.Q("DictionarySlot" + slotNumber);
+
+            if (item == null)
+            {
+                Debug.LogError("Could not find Slot" + slotNumber);
+                continue;
+            }
+
+            var notes = item.Q<TextField>("Notes" + slotNumber);
+            notes.RegisterValueChangedCallback(evt => {
+                NotesUpdate(evt.newValue, slotNumber - 1);
+            });
+            notes.isDelayed = true;
+
+            Slots.Add(item);
+        }
+
+        journalPage = notebookContents.Q<TextField>("JournalPage");
+        journalPage.RegisterValueChangedCallback(evt =>
+        {
+            PageUpdate(evt.newValue);
+        });
+
+        backPage = notebookContents.Q<Button>("BackPage");
+        backPage.RegisterCallback<ClickEvent>((e) => LoadPage(pageNumber - 1));
+
+        forwardPage = notebookContents.Q<Button>("ForwardPage");
+        forwardPage.RegisterCallback<ClickEvent>((e) => LoadPage(pageNumber + 1));
+
+        // Back button for dictionary
+        backButton = notebookContents.Q<Button>("BackButton");
+        backButton.RegisterCallback<ClickEvent>((e) => ToggleNotebook());
+
+        // Toggle to Dictionary
+        dictionaryButton = notebookContents.Q<Button>("DictionaryButton");
+        dictionaryButton.RegisterCallback<ClickEvent>((e) => ToggleJournalDictionary(true));
+
+        // Toggle to Journal
+        journalButton = notebookContents.Q<Button>("JournalButton");
+        journalButton.RegisterCallback<ClickEvent>((e) => ToggleJournalDictionary(false));
+
+        pageCount = notebookContents.Q<Label>("PageCount");
+
+        ToggleJournalDictionary(journalOrDict);
     }
 
     protected override sealed IEnumerator InteractLogic(PlayerController player)
@@ -293,6 +367,8 @@ public class NpcDialogue : Interactable
             PlayerController.Instance.CanMove = true;
 
             // Restore in-game UI
+            document.rootVisualElement.Q<VisualElement>("NotebookBox").style.display = DisplayStyle.None;
+
             document.rootVisualElement.style.visibility = Visibility.Hidden;
             document.rootVisualElement.style.display    = DisplayStyle.None;
 
@@ -386,5 +462,136 @@ public class NpcDialogue : Interactable
         }
 
         return "No Notes Available For This Word";
+    }
+
+    private void ToggleNotebook()
+    {
+        VisualElement notebookBox = document.rootVisualElement.Q<VisualElement>("NotebookBox");
+        if (notebookBox.style.display == DisplayStyle.None)
+        {
+            notebookBox.style.display = DisplayStyle.Flex;
+        }
+        else
+        {
+            notebookBox.style.display = DisplayStyle.None;
+        }
+    }
+
+    private void ToggleJournalDictionary(bool mode)
+    {
+        journalOrDict = mode;
+
+        if (journalOrDict) // Toggle to Dictionary
+        {
+            journalPage.style.display = DisplayStyle.None;
+            journalPage.parent.style.display = DisplayStyle.None;
+
+            foreach (VisualElement slot in Slots)
+            {
+                slot.style.display = DisplayStyle.Flex;
+            }
+        }
+        else // Toggle to Journal
+        {
+            foreach (VisualElement slot in Slots)
+            {
+                slot.style.display = DisplayStyle.None;
+            }
+            journalPage.style.display = DisplayStyle.Flex;
+            journalPage.parent.style.display = DisplayStyle.Flex;
+        }
+
+        LoadPage(0);
+    }
+
+    private void LoadPage(int pageNum)
+    {
+        if (pageNum < 0)
+        {
+            return;
+        }
+
+        if (journalOrDict)
+        {
+            LoadDictPage(pageNum);
+        }
+        else
+        {
+            LoadJournalPage(pageNum);
+        }
+    }
+
+    private void LoadDictPage(int pageNum)
+    {
+        PlayerController player = PlayerController.Instance;
+        if (pageNum > (int)player.dictionary.dictionaryList.Length / Slots.Count)
+        {
+            return;
+        }
+
+        pageCount.text = (pageNum + 1) + "/" + (((int)player.dictionary.dictionaryList.Length / Slots.Count) + 1);
+
+        int index = pageNum * Slots.Count;
+
+        pageNumber = pageNum;
+
+        foreach (VisualElement slot in Slots)
+        {
+            if (index >= player.dictionary.dictionaryList.Length)
+            {
+                slot.visible = false;
+                continue;
+            }
+
+            slot.visible = true;
+
+            var word = slot.Q<Label>("Word" + ((index % Slots.Count) + 1));
+            var notes = slot.Q<TextField>("Notes" + ((index % Slots.Count) + 1));
+
+            word.text = LanguageTable.PhoneticProcessor.Translate(player.dictionary.dictionaryList[index].Word);
+
+            if (player.dictionary.dictionaryList[index].Notes == "")
+            {
+                notes.value = "";
+                notes.textEdition.placeholder = "Notes...";
+            }
+            else
+            {
+                notes.value = player.dictionary.dictionaryList[index].Notes;
+            }
+            index++;
+        }
+    }
+
+    private void LoadJournalPage(int pageNum)
+    {
+        PlayerController player = PlayerController.Instance;
+        if (pageNum > player.dictionary.journalPages.Length)
+        {
+            return;
+        }
+
+        pageCount.text = (pageNum + 1) + "/" + (player.playerJournalSize);
+
+        pageNumber = pageNum;
+
+        journalPage.value = player.dictionary.journalPages[pageNum].Content;
+    }
+
+    private void NotesUpdate(string newValue, int index)
+    {
+        var notes = Slots[index].Q<TextField>("Notes" + (index + 1));
+        notes.value = newValue;
+
+        PlayerController player = PlayerController.Instance;
+        player.dictionary.dictionaryList[(pageNumber * Slots.Count) + index].Notes = newValue;
+    }
+
+    private void PageUpdate(string newValue)
+    {
+        journalPage.value = newValue;
+
+        PlayerController player = PlayerController.Instance;
+        player.dictionary.journalPages[pageNumber].Content = newValue;
     }
 }
