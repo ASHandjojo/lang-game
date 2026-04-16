1a2254adbc76249a57c31232e6d139a2d919061b
Matthew M. <blazinggunner@hotmail.com>
2026-04-16 12:47:28 -0500

    Adding ToolTipProperties, simplifying WordBox impl

--- Assets/Scripts/Interactables/DialogueBox.cs
+++ Assets/Scripts/Interactables/DialogueBox.cs
@@ -7,14 +7,83 @@ public sealed class DialogueBox : VisualElement
 
     private VisualElement wordTooltip;
 
+    private Label   dictWords, dictNotes;
     private WordBox hoverPrefab, noHoverPrefab;
 
-    public DialogueBox(VisualTreeAsset asset, Label dictWords, Label notes)
+    private StyleColor originalColor;
+
+    public DialogueBox(VisualTreeAsset asset, Label dictWords, Label dictNotes)
     {
         Debug.Assert(asset != null);
 
+        Debug.Assert(dictWords != null);
+        Debug.Assert(dictNotes != null);
+
+        this.dictWords = dictWords;
+        this.dictNotes = dictNotes;
+
         wordTooltip   = this.Q("WordTooltip");
-        hoverPrefab   = new WordBox(asset, wordTooltip, dictWords, notes, useHover: true);
-        noHoverPrefab = new WordBox(asset, wordTooltip, dictWords, notes, useHover: false);
+        hoverPrefab   = new WordBox(asset, MetaHover());
+        noHoverPrefab = new WordBox(asset);
     }
+
+    private ToolTipProperties MetaHover() => new()
+    {
+        onPointerEnter = (evt) =>
+        {
+            Label target = (Label)evt.target;
+            target.style.color = new StyleColor(Color.red);
+            ShowTooltip(name, evt.position, target.style.unityFontDefinition);
+        },
+        onPointerMove  = (evt) => MoveTooltip(evt.position),
+        onPointerLeave = (evt) =>
+            {
+                Label target = (Label)evt.target;
+                target.style.color = originalColor;
+                HideTooltip();
+            },
+        onPanelDetach  = (evt) => HideTooltip()
+    };
+
+    private string GetPlayerNotes(string word)
+    {
+        PlayerController player = PlayerController.Instance;
+        Dictionary dictionary   = player.dictionary;
+
+        foreach (DictionaryEntry entry in dictionary.dictionaryList) 
+        {
+            if (LanguageTable.PhoneticProcessor.Translate(entry.Word) == word) 
+            {
+                if (entry.Notes.Length == 0)
+                {
+                    return "No Notes Available For This Word";
+                }
+                return entry.Notes;
+            }
+        }
+
+        return "No Notes Available For This Word";
+    }
+
+    private void ShowTooltip(string name, Vector2 mousePosition, StyleFontDefinition font)
+    {
+        dictWords.text = name;
+        dictWords.style.unityFontDefinition = font;
+
+        dictNotes.text = GetPlayerNotes(name);
+
+        MoveTooltip(mousePosition);
+        wordTooltip.style.display = DisplayStyle.Flex;
+    }
+
+    private void MoveTooltip(Vector2 mousePosition)
+    {
+        float offsetX = 12.0f;
+        float offsetY = 12.0f;
+
+        wordTooltip.style.left = mousePosition.x + offsetX;
+        wordTooltip.style.top  = mousePosition.y + offsetY;
+    }
+
+    private void HideTooltip() => wordTooltip.style.display = DisplayStyle.None;
 }
--- Assets/Scripts/Interactables/WordBox.cs
+++ Assets/Scripts/Interactables/WordBox.cs
@@ -1,107 +1,38 @@
+using System;
+
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public struct ToolTipProperties
+{
+    public Action<PointerEnterEvent>    onPointerEnter;
+    public Action<PointerMoveEvent>     onPointerMove;
+    public Action<PointerLeaveEvent>    onPointerLeave;
+    public Action<DetachFromPanelEvent> onPanelDetach;
+}
+
 public sealed class WordBox : Label
 {
-    private VisualElement wordTooltip;
-    private Label word, notes;
-
-    private StyleColor originalColor;
-
-    public WordBox(VisualTreeAsset asset, VisualElement wordTooltip, Label word, Label notes, bool useHover)
+    private WordBox(VisualTreeAsset asset, ToolTipProperties? toolTipProps)
     {
         Debug.Assert(asset != null);
         asset.CloneTree(this);
-
-        Debug.Assert(wordTooltip != null);
-        Debug.Assert(word        != null);
-        Debug.Assert(notes       != null);
-
-        this.wordTooltip = wordTooltip;
-        this.word        = word;
-        this.notes       = notes;
-
-        originalColor = style.color;
 
         enableRichText       = true;
         parseEscapeSequences = true;
         AddToClassList("dialogue-text");
 
-        if (useHover)
+        if (toolTipProps != null)
         {
-            MetaHover();
+            var propsValue = toolTipProps.Value;
+            RegisterCallback<PointerEnterEvent>(new(propsValue.onPointerEnter));
+            RegisterCallback<PointerMoveEvent>(new(propsValue.onPointerMove));
+            RegisterCallback<PointerLeaveEvent>(new(propsValue.onPointerLeave));
+            RegisterCallback<DetachFromPanelEvent>(new(propsValue.onPanelDetach));
         }
     }
 
-    private void MetaHover()
-    {
-        RegisterCallback<PointerEnterEvent>(evt =>
-        {
-            Label target       = (Label) evt.target;
-            target.style.color = new StyleColor(Color.red);
-            ShowTooltip(name, evt.position, target.style.unityFontDefinition);
-        });
-
-        RegisterCallback<PointerMoveEvent>(evt =>
-        {
-            MoveTooltip(evt.position);
-        });
-
-        RegisterCallback<PointerLeaveEvent>(evt =>
-        {
-            Label target       = (Label) evt.target;
-            target.style.color = originalColor;
-            HideTooltip();
-        });
-        RegisterCallback<DetachFromPanelEvent>(evt =>
-        {
-            HideTooltip();
-        });
-    }
-
-    private string GetPlayerNotes(string word)
-    {
-        PlayerController player = PlayerController.Instance;
-        Dictionary dictionary   = player.dictionary;
-
-        foreach (DictionaryEntry entry in dictionary.dictionaryList) 
-        {
-            if (LanguageTable.PhoneticProcessor.Translate(entry.Word) == word) 
-            {
-                if (entry.Notes == "")
-                {
-                    return "No Notes Available For This Word";
-                }
-                return entry.Notes;
-            }
-        }
-
-        return "No Notes Available For This Word";
-    }
-
-
-    private void ShowTooltip(string name, Vector2 mousePosition, StyleFontDefinition font)
-    {
-        word.text = name;
-        word.style.unityFontDefinition = font;
-
-        notes.text = GetPlayerNotes(name);
-
-        MoveTooltip(mousePosition);
-        wordTooltip.style.display = DisplayStyle.Flex;
-    }
-
-    private void MoveTooltip(Vector2 mousePosition)
-    {
-        float offsetX = 12.0f;
-        float offsetY = 12.0f;
-
-        wordTooltip.style.left = mousePosition.x + offsetX;
-        wordTooltip.style.top  = mousePosition.y + offsetY;
-    }
-
-    private void HideTooltip()
-    {
-        wordTooltip.style.display = DisplayStyle.None;
-    }
+    public WordBox(VisualTreeAsset asset) : this(asset, null) {}
+    public WordBox(VisualTreeAsset asset, in ToolTipProperties props) :
+        this(asset, new ToolTipProperties?(props)) { }
 }
