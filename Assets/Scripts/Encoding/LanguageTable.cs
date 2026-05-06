c2f6a05baa8fe17ffef3f73c9f266ec9cc3f81ea
anakinrtran <anakinrtran@gmail.com>
2026-05-05 19:01:22 -0500

    synonymsystem rework
    made it separate from dictentry and InternalDictionary

--- Assets/Scripts/Encoding/LanguageTable.cs
+++ Assets/Scripts/Encoding/LanguageTable.cs
@@ -12,6 +12,7 @@ public sealed class LanguageTable : MonoBehaviour
 {
     [SerializeField] private StandardSignTable signTable;
     [SerializeField] private LigatureSub ligatureSub;
+    [SerializeField] private SynonymSystem synonymSystem;
 
     private PhoneticProcessor processor;
 
@@ -19,6 +20,7 @@ public sealed class LanguageTable : MonoBehaviour
 
     public static ReadOnlySpan<StandardSign> StandardSigns => Instance.signTable.entries;
     public static ReadOnlySpan<CompoundSign> CompoundSigns => Instance.ligatureSub.entries;
+    public static ReadOnlySpan<Synonyms> Synonyms => Instance.synonymSystem.synonymLists;
     public static ref readonly PhoneticProcessor PhoneticProcessor => ref Instance.processor;
 
     void Awake()
@@ -28,6 +30,11 @@ public sealed class LanguageTable : MonoBehaviour
             Debug.LogWarning($"Duplicate instance has been created of {nameof(LanguageTable)}! Destroying duplicate instance.");
             Destroy(this);
             return;
+        }
+    
+        foreach (var synonym in Synonyms)
+        {
+            Debug.Assert((int)synonym.wordType <= 3, "Must be N, Adj, V, or Adv");
         }
 
         DontDestroyOnLoad(this);
